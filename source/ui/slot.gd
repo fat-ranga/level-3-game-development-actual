ada00fac07494cb70539d9371aa91357c3cc97c5
fat-ranga <kael.robertson1257@gmail.com>
2023-11-04 06:32:06 +1300

    Tooltip

--- source/ui/slot.gd
+++ source/ui/slot.gd
@@ -3,12 +3,18 @@ extends Panel
 class_name Slot
 
 signal slot_clicked(index: int, button: int)
+signal tooltip_shown(slot_data: SlotData)
 
+var item_data_for_tooltip: ItemData
+var tool_tip: Tooltip
+
+@onready var tooltip_scene: PackedScene = preload("res://scenes/ui/tooltip.tscn")
 @onready var texture_rect: TextureRect = $MarginContainer/TextureRect
 @onready var quantity_label: Label = $QuantityLabel
 
 func set_slot_data(slot_data: SlotData) -> void:
 	var item_data = slot_data.item_data
+	item_data_for_tooltip = slot_data.item_data
 	texture_rect.texture = item_data.texture
 	# TODO tooltip maybe
 	
@@ -28,3 +34,12 @@ func _on_gui_input(event: InputEvent) -> void:
 			and event.is_pressed():
 				slot_clicked.emit(get_index(), event.button_index)
 		
+
+
+func _on_mouse_entered() -> void:
+	tool_tip = tooltip_scene.instantiate()
+	add_child(tool_tip)
+
+
+func _on_mouse_exited() -> void:
+	tool_tip.queue_free()
